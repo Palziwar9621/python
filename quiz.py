68228836d680eef9c3e2341a08973a5ff115136c
Palziwar9621 <156094787+Palziwar9621@users.noreply.github.com>
2026-03-24 16:35:08 +0530

    Add quiz functionality with multiple questions
    Implemented a quiz game with multiple questions and answers.

---
+++ quiz.py
@@ -0,0 +1,42 @@
+q=["Q:1) Who is the father of cell?","Q:2) At which place you can find edible food ?","Q:3) What is the %age of carbon in mild steel ?","Q:4) Who is the current president of India?","Q:5) In which compound's terms hardness of water is measured?"]
+a=["a) Robert Hooke","b) Mc Donalds","c) 0.15-0.3","a) Droupadi Murmu","d) CaCo3"]
+a1=["a) Robert Hooke","b) Plancks","c) Albert Einstein","d) None of these"]
+a2=["a) Yardley","b) Mc Donalds","c) Louis Vuitton","d) None of these"]
+a3=["a) 15-30%","b) 1.5-3%","c) 0.15-0.3%","d) None of these"]
+a4=["a) Droupadi Murmu", "b) Ram Nath Kovind","c) Pranab mukharjee","d) None of these"]
+a5=["a) MgCO3","b) CaSo4","c) MgSO4","d) CaCO3"]
+print(q[0],"\n", a1)
+x1=input()
+for a[0] in a:
+    if x1==a[0]:
+        print("Correct answer")
+    else:
+        break
+    print(q[1],"\n",a2)
+    x2=input()
+    for a[1] in a:
+        if x2==a[1]:
+            print("Correct answer")
+        else:
+            break
+        print(q[2],"\n",a3)
+        x3=input()
+        for a[2] in a:
+            if x3==a[2]:
+                print("Correct answer")
+            else:
+                break
+            print(q[3],"\n",a4)
+            x4=input()
+            for a[3] in a:
+                if x4==a[3]:
+                    print("Correct answer")
+                else:
+                    break
+                print(q(4),"\n",a5)
+                x5=input()
+                for a[4] in a:
+                    if x5==a[4]:
+                        print("Correct answer ")
+                    else:
+                        Break
